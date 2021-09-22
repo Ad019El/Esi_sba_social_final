70311b61ae9ce4f8a256ff364973e4d0e47959dd
Ad019El <a.dafi@esi-sba.dz>
2021-09-22 13:57:58 +0100

    Updates

---
+++ precache-manifest.8730506e280d02fa02e9a46b3745bb1b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59836042ee81775d0ded4dc930a6c25e",
+    "url": "/Esi_sba_social_final/index.html"
+  },
+  {
+    "revision": "f171796ffe3fa7ed276e",
+    "url": "/Esi_sba_social_final/static/css/main.e3271886.chunk.css"
+  },
+  {
+    "revision": "e90ab36e836fdc66b0cd",
+    "url": "/Esi_sba_social_final/static/js/2.65d66ca7.chunk.js"
+  },
+  {
+    "revision": "5bb71dd8729ff19ca9b7a076ba5bbb7f",
+    "url": "/Esi_sba_social_final/static/js/2.65d66ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f171796ffe3fa7ed276e",
+    "url": "/Esi_sba_social_final/static/js/main.61ed2b11.chunk.js"
+  },
+  {
+    "revision": "da49d92326ba006e26bf",
+    "url": "/Esi_sba_social_final/static/js/runtime-main.651f7b0d.js"
+  },
+  {
+    "revision": "50ee5008b770ac1e50c38cb39b84fd18",
+    "url": "/Esi_sba_social_final/static/media/Online.50ee5008.svg"
+  },
+  {
+    "revision": "4c763df01b43f707d2c8e2c0d7a91b9c",
+    "url": "/Esi_sba_social_final/static/media/chat.4c763df0.svg"
+  },
+  {
+    "revision": "31721699225819c35515bc3a7f91f795",
+    "url": "/Esi_sba_social_final/static/media/female_avatar.31721699.svg"
+  },
+  {
+    "revision": "7481a16a11dbba7dd882798db9d0b695",
+    "url": "/Esi_sba_social_final/static/media/logo avec script.7481a16a.png"
+  },
+  {
+    "revision": "2ea55d86e7fded9fe1b66c57a37d887f",
+    "url": "/Esi_sba_social_final/static/media/logo.2ea55d86.png"
+  },
+  {
+    "revision": "0c675689f904736df9b4408c1e33da49",
+    "url": "/Esi_sba_social_final/static/media/male_avatar.0c675689.svg"
+  },
+  {
+    "revision": "3df20eaa8e05916cbdbd59913d9c2265",
+    "url": "/Esi_sba_social_final/static/media/online-order.3df20eaa.svg"
+  },
+  {
+    "revision": "6db1fa0457f363178f217bd00483a072",
+    "url": "/Esi_sba_social_final/static/media/real_time.6db1fa04.svg"
+  },
+  {
+    "revision": "61010fd7c9e15e531b5baced885500b2",
+    "url": "/Esi_sba_social_final/static/media/settings.61010fd7.svg"
+  },
+  {
+    "revision": "f2fe2e5c9a95f84b719e7d1f6083c9b8",
+    "url": "/Esi_sba_social_final/static/media/smartphone.f2fe2e5c.svg"
+  },
+  {
+    "revision": "b42397fe14e67fc544bf1c2004f1bdd0",
+    "url": "/Esi_sba_social_final/static/media/user.b42397fe.svg"
+  }
+]);
